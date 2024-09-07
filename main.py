4c1b296e7bf19d85a200a00e792848279108d3f3
NanduriAnusha2801 <91455139+NanduriAnusha2801@users.noreply.github.com>
2024-09-07 16:39:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,142 @@
-### Print a welcome message
-print("Welcome to the Haunted Mansion!")
-print("You are a distant family member of a rich millionaire who has just passed way, leaving this mansion to you.")
-print("As the newfound owner, you decide to pay a visit to the mansion.")
-print("The house is dated, creaky, and falling apart. You walk in the front door.")
-print("Do you want to enter the living room or the dining room?")
+import os
+from PIL import Image
 
-### Prompt user for a choice
-roomChoice = input("> ")
+# Show map
+img = Image.open('Map.png')
+img.show()
 
-if(roomChoice == "living room"):
-  print("You enter the living room.")
-  print("As you walk in, you see a sleeping pitbull guarding some gold jewelry.")
-  print("Do you want to steal the jewelry from the pitbull?")
+# Display starting menu
+def prompt():
+    print("\t\tWelcome to my game\n\n\
+You must collect all six items before fighting the boss.\n\n\
+Moves:\t'go {direction}' (travel north, south, east, or west)\n\
+\t'get {item}' (add nearby item to inventory)\n")
 
-  pitBullChoice = input("> ")
+    input("Press any key to continue...")
 
-  if(pitBullChoice == "yes"):
-    print("You attempt to steal the jewelry, but it wakes up and rips you to shreds.")
-    print("You are now dead.")
-  elif(pitBullChoice == "no"):
-    print("You decide to not steal the dog's jewelry.")
-    print("You turn around and leave the house safely.")
-  else:
-    print("Invalid choice. Please enter yes or no.")
-elif(roomChoice == "dining room"):
-  print("You chose to go into the dining room.")
-  print("As you walk in, you see a shiny vase on the table.")
-  print("Do you want to open the vase?")
 
-  vaseChoice = input("> ")
+# Clear screen
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-  if(vaseChoice == "yes"):
-    print("You open the vase and find a pile of bones!")
-  elif(vaseChoice == "no"):
-    print("You decide not to open the shiny vase.")
-    print("As you turn to leave, you hear a cracking sound coming from the corner.")
-    print("A dark figure with glowing red eyes launches at you, knocking you unconcious")
-    print("You wake up in your bed. It was all a dream.")
-  else:
-    print("Invalid choice. Please enter yes or no.")
-else:
-  print("Invalid choice. Please enter living room or dining room.")
+
+# Map
+rooms = {
+    'Liminal Space': {'North': 'Mirror Maze', 'South': 'Bat Cavern', 'East': 'Bazaar'},
+    'Mirror Maze': {'South': 'Liminal Space', 'Item': 'Crystal'},
+    'Bat Cavern': {'North': 'Liminal Space', 'East': 'Volcano', 'Item': 'Staff'},
+    'Bazaar' : {'West': 'Liminal Space', 'North': 'Meat Locker', 'East': 'Dojo', 'Item': 'Altoids'},
+    'Meat Locker' : {'South': 'Bazaar', 'East': 'Quicksand Pit', 'Item': 'Fig'},
+    'Quicksand Pit': {'West': 'Meat Locker', 'Item': 'Robe'},
+    'Volcano': {'West': 'Bat Cavern', 'Item': 'Elderberry'},
+    'Dojo': {'West': 'Bazaar', 'Boss': 'Shadow Man'}
+    }
+
+# List of vowels
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+# List to track inventory
+inventory = []
+
+# Tracks current room
+current_room = "Liminal Space"
+
+# Tracks last move
+msg = ""
+
+clear()
+prompt()
+
+# Gameplay loop
+while True:
+
+    clear()
+
+    # Display player info
+    print(f"You are in the {current_room}\nInventory : {inventory}\n{'-' * 27}")
+
+    # Display msg
+    print(msg)
+
+    # Item indicator
+    if "Item" in rooms[current_room].keys():
+
+        nearby_item = rooms[current_room]["Item"]
+
+        if nearby_item not in inventory:
+
+            if nearby_item[-1] == 's':
+                print(f"You see {nearby_item}")
+
+            elif nearby_item[0] in vowels:
+                print(f"You see an {nearby_item}")
+
+            else:
+                print(f"You see a {nearby_item}")
+    
+    # Boss encounter
+    if "Boss" in rooms[current_room].keys():
+
+        if len(inventory) < 6:
+            print(f"You lost a fight with {rooms[current_room]['Boss']}.")
+            break
+
+        else:
+            print(f"You beat {rooms[current_room]['Boss']}!")
+            break
+
+    # Accepts player's move as input
+    user_input = input("Enter your move:\n")
+
+    # Splits move into words
+    next_move = user_input.split(' ')
+
+    # First word is action
+    action = next_move[0].title()
+
+    # Reset item and direction
+    item = "Item"
+    direction = "null"
+
+    # Second word is object or direction
+    if len(next_move) > 1:
+        item = next_move[1:]
+        direction = next_move[1].title()
+
+        item = " ".join(item).title()
+
+    # Moving between rooms
+    if action == "Go":
+
+        try:
+            current_room = rooms[current_room][direction]
+            msg = f"You travel {direction}"
+
+        except:
+            msg = "You can't go that way."
+    
+    # Picking up items
+    elif action == "Get":
+        try:
+            if item == rooms[current_room]["Item"]:
+
+                if item not in inventory:
+
+                    inventory.append(rooms[current_room]["Item"])
+                    msg = f"{item} retrieved!"
+
+                else:
+                    msg = f"You already have the {item}"
+            
+            else:
+                msg = f"Can't find {item}"
+        except:
+            msg = f"Can't find {item}"
+    
+    # Exit program
+    elif action == "Exit":
+        break
+
+    # Any other commands invalid
+    else:
+        msg = "Invalid command"
